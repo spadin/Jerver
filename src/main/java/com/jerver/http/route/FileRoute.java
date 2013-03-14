a68d22b38f921f4fcf5c7a728867099051f6de2a
Sandro Padin <sandropadin@gmail.com>
2013-03-13 19:26:32 -0500

    Adding FileRoute

---
+++ src/main/java/com/jerver/http/route/FileRoute.java
@@ -0,0 +1,39 @@
+package com.jerver.http.route;
+
+import com.jerver.http.request.Request;
+import com.jerver.http.response.Response;
+import com.jerver.mime.MimeType;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileRoute implements Routable {
+    private final Path filePath;
+    protected static final MimeType mimeType = MimeType.INSTANCE;
+
+    public FileRoute(Path filePath) {
+        this.filePath = filePath;
+    }
+
+    public byte[] getBody() {
+        byte[] body = null;
+        try {
+            body = Files.readAllBytes(filePath);
+        } catch (IOException e) {
+            System.out.println("Failed to open file: " + e.getMessage());
+        }
+
+        return body;
+    }
+
+    public void resolve(Request request, Response response) {
+        response.setStatusCode(200);
+        response.appendHeader("Content-Type: " + getContentType());
+        response.setBody(getBody());
+    }
+
+    public String getContentType() {
+        return mimeType.getForFilename(filePath.getFileName().toString());
+    }
+}
---
+++ src/test/java/com/jerver/http/route/FileRouteTest.java
@@ -0,0 +1,36 @@
+package com.jerver.http.route;
+
+import com.jerver.http.mock.MockRequest;
+import com.jerver.http.mock.MockResponse;
+import com.jerver.http.request.Request;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class FileRouteTest {
+    @Test
+    public void testPlaintextContentType() throws Exception {
+        FileRoute route = new FileRoute(Paths.get("resources/test.txt"));
+        assertEquals("text/plain", route.getContentType());
+    }
+
+    @Test
+    public void testHtmlContentType() throws Exception {
+        FileRoute route = new FileRoute(Paths.get("resources/test.html"));
+        assertEquals("text/html", route.getContentType());
+    }
+
+    @Test
+    public void testPlaintextFileRoute() throws Exception {
+        FileRoute route = new FileRoute(Paths.get("resources/test.txt"));
+        Request request = new Request();
+        MockResponse response = new MockResponse();
+        route.resolve(request, response);
+
+        assertThat(response.getResponseText(), containsString("This is a test file"));
+    }
+}
