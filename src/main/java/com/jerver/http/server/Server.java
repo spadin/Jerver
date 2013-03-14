bdb7c2f7e77e606eb42fbd86a9ce43b05b9e1e55
Sandro Padin <sandropadin@gmail.com>
2013-03-14 15:46:33 -0500

    Reads options for publicDirectory and port

--- src/main/java/com/jerver/http/server/Server.java
+++ src/main/java/com/jerver/http/server/Server.java
@@ -2,11 +2,13 @@ package com.jerver.http.server;
 
 import com.jerver.http.route.*;
 import com.jerver.resource.Resource;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Server {
     private static final Router router = Router.INSTANCE;
@@ -28,13 +30,38 @@ public class Server {
         }
     }
 
-    public static void main(String[]args) {
-        router.setPublicDirectory("src/test/resources");
+    public static void main(String[] args) {
+        String publicDir = getOption("-d", "", args);
+        int port = getOption("-p", 9999, args);
+
+        router.setPublicDirectory(publicDir);
         router.addRoute("GET", "/hello", "Hello World!");
         router.addRoute("GET", "/time", new SleepyRoute(1000));
         router.addRoute("GET", "/form", new StringRoute(Resource.getStringForResource("form.html"), "text/html"));
         router.addRoute("POST", "/form", new StringSubstitutionRoute(Resource.getStringForResource("form.jrv.html")));
 
-        new Server(9999);
+        new Server(port);
+    }
+
+    public static String getOption(String code, String def, String[] args) {
+        Scanner s = new Scanner(StringUtils.join(args, " "));
+        String option = def;
+        if(s.findInLine(code) != null) {
+            option = s.next();
+        }
+        return option;
+    }
+
+    public static int getOption(String code, int def, String[] args) {
+        String optStr = getOption(code, def+"", args);
+        int opt = def;
+
+        try {
+            opt = Integer.parseInt(optStr);
+        } catch (NumberFormatException e) {
+            System.out.println(code + " option must be numeric");
+        }
+
+        return opt;
     }
 }
--- src/test/java/com/jerver/http/server/ServerTest.java
+++ src/test/java/com/jerver/http/server/ServerTest.java
@@ -2,9 +2,43 @@ package com.jerver.http.server;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ServerTest {
     @Test
     public void testConnection() {
 
+    }
+
+    @Test
+    public void testGetOptionWhenSet() throws Exception {
+        String[] args = {"-p", "9898", "-d", "/some/cool/path"};
+        String opt = Server.getOption("-d", "/src/test/resources", args);
+
+        assertEquals("/some/cool/path", opt);
+    }
+
+    @Test
+    public void testGetOptionWhenNotSet() throws Exception {
+        String[] args = {"-p", "9898"};
+        String opt = Server.getOption("-d", "/src/test/resources", args);
+
+        assertEquals("/src/test/resources", opt);
+    }
+
+    @Test
+    public void testGetOptionWhenNothingIsSet() throws Exception {
+        String[] args = {};
+        String opt = Server.getOption("-d", "/src/test/resources", args);
+
+        assertEquals("/src/test/resources", opt);
+    }
+
+    @Test
+    public void testGetOptionAsInt() throws Exception {
+        String[] args = {"-p", "9997"};
+        int opt = Server.getOption("-p", 9999, args);
+
+        assertEquals(9997, opt);
     }
 }
